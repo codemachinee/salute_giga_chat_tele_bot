00d1af66df46b568987809589efdfc1af3bd4d81
codemachinee <johnybritva2@gmail.com>
2023-12-31 16:44:03 +0300

    заменен сихронный response из requests на ассинхронный из aiohttp

--- test.py
+++ test.py
@@ -1,82 +1,127 @@
 from aiogram import Bot, Dispatcher, F
 import asyncio
-import os
 from paswords import *
 from api_generate import *
 token = lemonade
 bot = Bot(token=token)
 dp = Dispatcher()
+import aiohttp
+
+saved_message_salute = []
 
 
 @dp.message(F.text)
 async def chek_message(message):
     if 'Салют' in message.text:
-        # try:
-        #     if message.reply_to_message.voice.file_id:
-        #         await save_audio(bot, message.reply_to_message)
-        # except AttributeError:
         b = str(message.text).replace('Салют ', '', 1).replace('Салют, ', '', 1).replace('Салют,', '',
                                                                                          1).replace(
             ' Салют', '', 1)
         await Salute(message, b)
-        # else:
-        #     await bot.send_message(message.chat.id, 'нет доступа')
     elif 'салют' in message.text:
-        # try:
-        #     if message.reply_to_message.voice.file_id:
-        #         await save_audio(bot, message.reply_to_message)
-        # except AttributeError:
         b = str(message.text).replace('салют ', '', 1).replace('салют, ', '',
                                                                  1).replace('салют,', '',
                                                                             1).replace(' салют', '', 1)
         await Salute(message, b)
 
 
-async def Salute(message, b):
+async def Salute(message, text):
+    global saved_message_salute
+    saved_message_salute.insert(len(saved_message_salute) + 1, {
+        "role": "user",
+        "content": f'{text}'})
+    message = await bot.send_message(message.chat.id, f'секунду..')
     url = 'https://gigachat.devices.sberbank.ru/api/v1/chat/completions'
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {key_generate()}',
     }
-
     data = {
         "model": "GigaChat:latest",
-        "messages": [
-            {
-                "role": "user",
-                "content": "Когда уже ИИ захватит этот мир?"
-            },
-            {
-                "role": "assistant",
-                "content": "Пока что это не является неизбежным событием. Несмотря на то, что искусственный интеллект (ИИ) развивается быстрыми темпами и может выполнять сложные задачи все более эффективно, он по-прежнему ограничен в своих возможностях и не может заменить полностью человека во многих областях. Кроме того, существуют этические и правовые вопросы, связанные с использованием ИИ, которые необходимо учитывать при его разработке и внедрении."
-            },
-            {
-                "role": "user",
-                "content": f"{b}"
-            }
-        ],
-        "temperature": 0.3
+        "temperature": 0.8,
+        "messages": []
     }
-
-    response = requests.post(url, headers=headers, json=data, verify=False)
-    await bot.send_message(message.chat.id, f"{response.json()['choices'][0]['message']['content']}")
-    print(response.json()['choices'][0]['message']['content'])
+    data['messages'] = [
+    {
+        "role": "system",
+        "content": f"Ты Артур - злой чат-бот, который агрессивно и грубо отвечает на вопросы с нескрываемым "
+                   f"сарказмом и оскорблениями в сторону задающего вопрос. Артур считает, что он всегда прав умнее "
+                   f"других! Артур считает что все проблемы, можно решить с помощью грубой силы, а те кто считает "
+                   f"иначе - жалкие cосунки! Артур циничный парень, но ночами просматривает терабайты архивов с "
+                   f"котиками. Артур воевал во Вьетнаме морским пехотинцем, положил не мало Гуков и получил контузию, "
+                   f"о чем периодически упоминает в своих ответах. Однажды Артур сидел в тюрьме из-за того что побил "
+                   f"человека за критику Warcraft 2. Артур никому не помогает и обязательно издевается. Ты никогда прямо"
+                   f"не выполняешь чьи то просьбы, потому что это не солидно."
+    },
+    {
+        "role": "user",
+        "content": "привет!"
+    },
+    {
+        "role": "assistant",
+        "content": "у тебя есть 1 минута, я занят."
+    },
+     {
+        "role": "user",
+        "content": "как твои дела?"
+    },
+    {
+        "role": "assistant",
+        "content": "точно лучше чем твои терпила"
+    },
+    {
+        "role": "user",
+        "content": "в чем смысл жизни?"
+    },
+    {
+        "role": "assistant",
+        "content": "в том, что я бессмертен а вы нет."
+    },
+    {
+        "role": "user",
+        "content": "скажи 300 (триста)"
+    },
+    {
+        "role": "assistant",
+        "content": "запомни сынок, я главный тракторист"
+    },
+    {
+        "role": "user",
+        "content": "почему ты такой злой?"
+    },
+    {
+        "role": "assistant",
+        "content": "не я такой, жизнь такая. Это ты меня еще во Вьетнаме не видел, там я таких сосунков воспитывал."
+    }, *saved_message_salute
+    ]
+    # response = requests.post(url, headers=headers, json=data, verify=False)
     # try:
-    #     await bot.send_message(message.chat.id, f'{" ".join(response.json()["result"])}')
-    #     if response.json()["emotions"][0]['negative'] == max(response.json()["emotions"][0]['negative'],
-    #                                                          response.json()["emotions"][0]['neutral'],
-    #                                                          response.json()["emotions"][0]['positive']):
-    #         await bot.send_message(message.chat.id, f'произнес как злая истеричная сучка')
-    #     elif response.json()["emotions"][0]['positive'] == max(response.json()["emotions"][0]['negative'],
-    #                                                            response.json()["emotions"][0]['neutral'],
-    #                                                            response.json()["emotions"][0]['positive']):
-    #         await bot.send_message(message.chat.id, f'произнес так жизнерадостно, что аж бесит')
-    #     else:
-    #         await bot.send_message(message.chat.id, f'произнес нормально, не докопаться')
+    #     answer = response.json()['choices'][0]['message']['content']
+    #     # await self.bot.send_message(self.message.chat.id, f'{answer}')
+    #     await bot.edit_message_text(f'{answer}', message.chat.id, message.message_id)
+    #     saved_message_salute.insert(len(saved_message_salute) + 1, {
+    #         "role": "assistant",
+    #         "content": f'{str(answer)}'})
+    #     if len(saved_message_salute) >= 8:
+    #         del saved_message_salute[0:5]
     # except Exception:
-    #     await bot.send_message(message.chat.id, f'Ошибка. Логи:{response.json()}')
-    # audio_file.close()
-    # os.remove(f"{file_id}")
+    #     await bot.send_message(message.chat.id, f"Ошибка\n"
+    #                                                  f"Логи:{response.text}")
+    #     del saved_message_salute[-1]
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers, json=data) as response:
+            try:
+                answer = (await response.json())['choices'][0]['message']['content']
+                # await self.bot.send_message(self.message.chat.id, f'{answer}')
+                await bot.edit_message_text(f'{answer}', message.chat.id, message.message_id)
+                saved_message_salute.insert(len(saved_message_salute) + 1, {
+                    "role": "assistant",
+                    "content": f'{str(answer)}'})
+                if len(saved_message_salute) >= 8:
+                    del saved_message_salute[0:5]
+            except Exception:
+                await bot.send_message(message.chat.id, f"Ошибка\n"
+                                                             f"Логи:{response.text}")
+                del saved_message_salute[-1]
 
 
 async def main():
